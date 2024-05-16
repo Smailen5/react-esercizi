357629f71f4001cc0ea45f97859eb5d18510d70b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-05-16 16:03:17 +0200

    aggiornato le larghezze per il layout desktop

--- appuntamenti/inizio/src/App.jsx
+++ appuntamenti/inizio/src/App.jsx
@@ -5,32 +5,36 @@ import { useState } from "react";
 function App() {
   const [persona, setPersona] = useState(data);
   // Per eliminare tutto setto setPersona a un array vuoto
-  const deleteAll = ()=>{
-    setPersona([])
-  }
+  const deleteAll = () => {
+    setPersona([]);
+  };
   // Per ricaricare passo a setPersona di nuovo tutti i dati
-  const reload = ()=>{
-    setPersona(data)
-  }
+  const reload = () => {
+    setPersona(data);
+  };
   // Metto dentro una variabile tutti gli elementi che hanno un id diverso da quello selezionato, poi lo passo a setPersona
-  const removeItem = (id)=>{
-    let newPeople = persona.filter((el) => el.id !== id)
-    setPersona(newPeople)
-  }
+  const removeItem = (id) => {
+    let newPeople = persona.filter((el) => el.id !== id);
+    setPersona(newPeople);
+  };
   return (
     <>
       <div className="flex flex-col justify-center items-center text-blue-200 m-6">
         <h2 className="font-bold capitalize text-4xl">Prossimi incontri</h2>
         {/* removeItem e persona vengono passate come props cosi possono accedervi */}
         <List data={persona} removeItem={removeItem} />
-        <div className="flex w-full justify-between">
-          <button onClick={reload}
+        <div className="flex w-full justify-between lg:max-w-sm">
+          <button
+            onClick={reload}
             className="bg-blue-500 px-5 py-1 rounded-xl uppercase text-xxs font-bold"
           >
-            reload
+            ricarica
           </button>
-          <button onClick={deleteAll} className="rounded-xl border-2 px-5 py-1 border-red-500 uppercase text-xxs font-bold text-red-500">
-            delete all
+          <button
+            onClick={deleteAll}
+            className="rounded-xl border-2 px-5 py-1 border-red-500 uppercase text-xxs font-bold text-red-500"
+          >
+            elimina tutto
           </button>
         </div>
       </div>
--- appuntamenti/inizio/src/components/List.jsx
+++ appuntamenti/inizio/src/components/List.jsx
@@ -1,14 +1,17 @@
-const List = ({data, removeItem}) => {
-//   console.log(props.data);
+const List = ({ data, removeItem }) => {
+  //   console.log(props.data);
   return (
     <>
-      <section className="bg-neutral-800 rounded-md shadow-md shadow-black my-10">
+      <section className="bg-neutral-800 rounded-md shadow-md shadow-black my-10 lg:max-w-sm">
         <ul className="flex flex-col gap-4 ">
           {/* Faccio il map dei data in arrivo */}
           {data.map((persona) => {
             return (
               // imposto un key univoco usando id del oggetto data che ricevo
-              <li key={persona.id} className="flex items-center gap-4 p-4">
+              <li
+                key={persona.id}
+                className="flex items-center gap-4 p-4"
+              >
                 {/* Passo come props la funzione removeItem, passo anche tutti i dati a Person */}
                 <Person {...persona} removeItem={removeItem} />
               </li>
