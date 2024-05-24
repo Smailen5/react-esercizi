c6ad6b6ff48b6685cd07bebf9e4f07e97497a7cf
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-05-24 14:45:58 +0200

    impostato il progetto con react+vite con tailwind

---
+++ vacanze/src/App.jsx
@@ -0,0 +1,12 @@
+// import Title from "./components/Title";
+// import Holiday from "./components/Holiday";
+//Ho utilizzato Cartella Component per dare una chiara locazione ai miei componenti
+function App() {
+  return (
+    <section className="min-h-screen bg-neutral-900 flex flex-col items-center">
+      <h2 className="text-white text-xl font-semibold capitalize ">le nostre vacanze</h2>
+    </section>
+  );
+}
+
+export default App;
