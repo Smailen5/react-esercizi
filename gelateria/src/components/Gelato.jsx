62d795207273f75b3524f85001139f97f24b4582
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-06-05 21:06:52 +0200

    corretto un bug che non permetteva una visualizzazione corretta su smartphone reali

--- gelateria/src/components/Gelato.jsx
+++ gelateria/src/components/Gelato.jsx
@@ -5,11 +5,11 @@ const Gelato = ({ id, nome, img, descrizione, prezzo, categoria }) => (
     key={id}
     className="group w-full flex gap-4 p-3 bg-sky-100 text-black shadow-lg rounded hover:bg-sky-400/[0.4] hover:scale-105 transition ease-linear lg:p-4"
   >
-    <div className="min-w-32 h-24 p-1 bg-white rounded-md shadow-md group-hover:scale-110 delay-300 transition-transform duration-500 md:group-hover:animate-bounce lg:min-w-44 lg:h-32">
+    <div className=" w-full max-w-32 md:w-32 h-24 p-1 bg-white rounded-md shadow-md group-hover:scale-110 delay-300 transition-transform duration-500 md:group-hover:animate-bounce lg:min-w-44 lg:h-32">
       <img
         src={img}
         alt={nome}
-        className="rounded-md m-auto w-full h-full object-cover "
+        className="rounded-md w-full h-full object-cover "
       />
     </div>
     <section>
