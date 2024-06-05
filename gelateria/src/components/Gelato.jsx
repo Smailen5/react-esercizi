72e851182fe13664fd5041c8d2bb58ae36be55d6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-06-05 21:29:44 +0200

    finalmente risolto completamente la visualizzazione delle immagini su iphone

--- gelateria/src/components/Gelato.jsx
+++ gelateria/src/components/Gelato.jsx
@@ -3,16 +3,16 @@ import PropTypes from "prop-types";
 const Gelato = ({ id, nome, img, descrizione, prezzo, categoria }) => (
   <article
     key={id}
-    className="group w-full flex gap-4 p-3 bg-sky-100 text-black shadow-lg rounded hover:bg-sky-400/[0.4] hover:scale-105 transition ease-linear lg:p-4"
+    className="group w-full flex items-center  gap-4 p-3 bg-sky-100 text-black shadow-lg rounded hover:bg-sky-400/[0.4] hover:scale-105 transition ease-linear lg:p-4"
   >
-    <div className=" w-full max-w-32 md:w-32 h-24 p-1 bg-white rounded-md shadow-md group-hover:scale-110 delay-300 transition-transform duration-500 md:group-hover:animate-bounce lg:min-w-44 lg:h-32">
+    <div className=" min-w-32 max-w-32 h-24 p-1 bg-white rounded-md shadow-md group-hover:scale-110 delay-300 transition-transform duration-500 md:group-hover:animate-bounce lg:min-w-44 lg:h-32">
       <img
         src={img}
         alt={nome}
         className="rounded-md w-full h-full object-cover "
       />
     </div>
-    <section>
+    <section className="flex-grow">
       <header className="flex justify-between items-start">
         <div className="flex flex-col">
           <h5 className="text-sm font-bold xl:text-base">{nome}</h5>
