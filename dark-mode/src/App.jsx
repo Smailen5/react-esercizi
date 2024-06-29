6da9ddf45a554133cf988c16b360c8cba1e1b1c6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-06-29 19:56:18 +0200

    - riorganizzato le classi secondo le best practice di tailwindcss

--- dark-mode/src/App.jsx
+++ dark-mode/src/App.jsx
@@ -29,11 +29,11 @@ function App() {
   return (
     <>
       <div className="min-h-svh dark:bg-neutral-900">
-        <div className="text-center py-4 mb-10">
+        <div className="mb-10 py-4 text-center">
           {/* <h2>DARK MODE APP</h2> */}
           <button
             onClick={() => setTheme(!theme)}
-            className="bg-blue-400 text-black shadow-xl hover:text-blue-400 hover:bg-black dark:bg-red-500 dark:text-white dark:hover:bg-white dark:hover:text-red-500"
+            className="bg-blue-400 text-black shadow-xl hover:bg-black hover:text-blue-400 dark:bg-red-500 dark:text-white dark:hover:bg-white dark:hover:text-red-500"
             style={{
               // Questi valori sono comuni per entrambi i tasti
               fontSize: "0.75rem",
@@ -48,11 +48,11 @@ function App() {
           >
             cambia modalita
           </button>
-          <p className="text-black dark:text-white py-4">
+          <p className="py-4 text-black dark:text-white">
             {theme ? "dark " : "light "}mode attiva
           </p>
         </div>
-        <section className="flex justify-center mt-2">
+        <section className="mt-2 flex justify-center">
           <Articolo data={database} />
         </section>
       </div>
--- dark-mode/src/components/Articolo.jsx
+++ dark-mode/src/components/Articolo.jsx
@@ -4,17 +4,17 @@
 const Articolo = ({ data }) => {
   //   console.log(data);
   return (
-    <article className="grid grid-cols-2 gap-8 max-w-lg">
+    <article className="grid max-w-lg grid-cols-2 gap-8">
       {data.map((el) => {
         const { id, title, body } = el;
         return (
           <div
             key={id}
-            className=" bg-white text-black shadow-xl dark:bg-neutral-700 dark:text-white rounded-md p-4 text-center grid gap-4"
+            className="grid gap-4 rounded-md bg-white p-4 text-center text-black shadow-xl dark:bg-neutral-700 dark:text-white"
           >
-            <h6 className="font-bold text-lg capitalize flex flex-col items-center">
+            <h6 className="flex flex-col items-center text-lg font-bold capitalize">
               {title}
-              <hr className="border-red-500 w-1/4"></hr>
+              <hr className="w-1/4 border-red-500"></hr>
             </h6>
 
             <p className="text-sm">{body}</p>
