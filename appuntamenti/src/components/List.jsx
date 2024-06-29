be1001f222cffdcce1a44a6e719525b0263e6e7d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-06-29 19:54:48 +0200

    - riorganizzato le classi secondo le best practice di tailwindcss

--- appuntamenti/src/components/List.jsx
+++ appuntamenti/src/components/List.jsx
@@ -5,8 +5,8 @@ const List = ({ data, removeItem }) => {
   //   console.log(props.data);
   return (
     <>
-      <section className="bg-neutral-800 rounded-md shadow-md shadow-black my-10 ">
-        <ul className="flex flex-col gap-4 ">
+      <section className="my-10 rounded-md bg-neutral-800 shadow-md shadow-black">
+        <ul className="flex flex-col gap-4">
           {/* Faccio il map dei data in arrivo */}
           {data.map((persona) => {
             return (
@@ -28,13 +28,13 @@ export default List;
 const Person = ({ nome, stato, img, removeItem, id }) => {
   return (
     <>
-      <img src={img} alt={"link: " + img} className="rounded-full w-12 h-12" />
+      <img src={img} alt={"link: " + img} className="h-12 w-12 rounded-full" />
       <div className="w-full">
         <div className="flex justify-between pb-1">
           <h2 className="text-sm font-bold">{nome}</h2>
           <button
             onClick={() => removeItem(id)}
-            className="text-red-500 rounded-lg p-1 hover:text-white hover:bg-red-500"
+            className="rounded-lg p-1 text-red-500 hover:bg-red-500 hover:text-white"
           >
             <RiDeleteBack2Fill />
           </button>
