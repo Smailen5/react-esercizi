c91a6b4eb0084149de52cce33e1517780c738a55
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-04 12:41:21 +0200

    - creato il carrello in CartItem

--- cart/src/components/CartItem.js
+++ cart/src/components/CartItem.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
+import { BiPlus, BiMinus } from "react-icons/bi";
 
-const CartItem = () => {
-  return <article className="cart-item">CartItem</article>;
+const CartItem = ({ id, image, price, name, countInStock }) => {
+  return (
+    <article className="cart-item">
+      {/* immagine */}
+      <div className="img-container">
+        <img src={image} alt={name} className="img" />
+      </div>
+      <p className="prd-name">{name}</p>
+
+      {/* selettore quantità */}
+      <div className="qty-selector">
+        <button className="btn icon-btn">
+          <BiPlus className="icon" />
+        </button>
+        <p>1</p>
+        <button className="btn icon-btn">
+          <BiMinus className="icon minus-icon" />
+        </button>
+      </div>
+
+      {/* prezzo */}
+      <p>{price}€</p>
+      <button className="btn icon-btn">
+        <MdDelete className="icon minus-icon" />
+      </button>
+    </article>
+  );
 };
 
 export default CartItem;
