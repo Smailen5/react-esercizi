90ab288058a3f2e4af2da75695596910c4d05a5a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-10 16:25:53 +0200

    creato section team in AboutScreen

--- cocktail/src/screen/AboutScreen.jsx
+++ cocktail/src/screen/AboutScreen.jsx
@@ -12,7 +12,7 @@ const AboutScreen = () => {
       <Hero img={image}>
         <section>
           <div className="mb-4 h-px bg-pink-500" />
-          <h3 className="text-center italic text-white text-xl">
+          <h3 className="text-center text-xl italic text-white">
             <q>
               Il cocktail perfetto è un&apos;armonia di sapori <br /> da gustare
               con piacere e senza fretta.
@@ -22,19 +22,30 @@ const AboutScreen = () => {
         </section>
       </Hero>
 
-      <section className=" bg-gray-100 p-8 flex justify-center">
+      {/* section card info */}
+      <section className="flex justify-center bg-gray-100 p-8">
         <div className="container">
-          <h3 className="pb-8 text-center font-semibold uppercase text-violet-700 text-xl">
+          <h3 className="pb-8 text-center text-xl font-semibold uppercase text-violet-700">
             il nostro progetto
           </h3>
           <div className="card-section flex flex-col gap-8">
             {valueCards.map((card) => {
               return (
-                <Card key={card.title} {...card} className="value-card">
-                </Card>
+                <Card key={card.title} {...card} className="value-card"></Card>
               );
             })}
           </div>
+        </div>
+      </section>
+
+      {/* section team */}
+      <section className="relative flex h-screen justify-center bg-red-400">
+        <div className='relative h-[70vh] w-full'>
+          <div
+            className="absolute inset-0 h-[70vh] bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${teamImg})` }}
+          />
+          <div className="absolute bottom-0 h-20 w-full bg-white" />
         </div>
       </section>
       <Footer />
