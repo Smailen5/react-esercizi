8d7b3aa7f9b6ab39d50317412b64ec2dd17e9531
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-11 17:28:06 +0200

    aggiunge il testo per ContactScreen - aggiunge lo stile al testo

--- cocktail/src/screen/ContactScreen.jsx
+++ cocktail/src/screen/ContactScreen.jsx
@@ -6,7 +6,22 @@ const ContactScreen = () => {
   return (
     <>
       <Navbar />
-      ContactScreen
+      <Hero img={backImage} disableOverlay>
+        <div className="flex h-screen flex-col p-8">
+          {/* sezione info aggiunta cocktail */}
+          <div className="grid gap-4 text-center">
+            <h4 className="text-4xl font-bold tracking-wide text-slate-900">
+              Vorresti aggiungere un cocktail?
+            </h4>
+            <p className="text-lg leading-6 text-slate-400">
+              il nostro team e sempre disponibile per valutare possibili nuove
+              ricette ed ad aggiungerle alla nostro database
+            </p>
+          </div>
+          {/* form per aggiunta cocktail */}
+          <div className="h-96 w-full bg-red-500 "></div>
+        </div>
+      </Hero>
       <Footer />
     </>
   );
