3100e12798ba9ffbe6bb3b815b47c54116998b1c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-11 16:58:01 +0200

    rimuove lo styled components da Card

--- cocktail/src/components/Card.jsx
+++ cocktail/src/components/Card.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-// import styled from "styled-components";
 const Card = ({ title, text, icon, className, subtitle, img }) => {
   return (
     // se className e definito prende il valore, altrimenti lascia vuoto
@@ -26,6 +23,7 @@ const Card = ({ title, text, icon, className, subtitle, img }) => {
         ) : (
           <div className="">
             <div
+              // togliere ${title !== "Tatyana Doglieva" && "bg-top"} quando aggiungerai nuove immagini
               className={`h-52 w-52 rounded-full bg-cover ${title !== "Tatyana Doglieva" && "bg-top"}`}
               style={{ backgroundImage: `url(${img})` }}
             ></div>
@@ -36,48 +34,5 @@ const Card = ({ title, text, icon, className, subtitle, img }) => {
     </article>
   );
 };
-
-// const Wrapper = styled.article`
-//   padding-top: 2rem;
-//   padding-bottom: 2rem;
-//   background-color: white;
-//   border-radius: var(--radius);
-//   transition: var(--transition);
-//   .value-card:hover {
-//     cursor: pointer;
-//     transform: scale(1.03);
-//     background-color: var(--trypan-blue-2);
-//     .card-title {
-//       color: white;
-//     }
-//     .card-text p {
-//       color: var(--extra-light-gray);
-//     }
-//   }
-//   .card {
-//     display: grid;
-//     place-items: center;
-//     text-align: center;
-//     gap: 1rem;
-//     .card-subtitle {
-//       font-size: 1.1rem !important;
-//       color: var(--dark-gray);
-//     }
-//     .card-icon {
-//       font-size: 2rem !important;
-//       padding: 0;
-//     }
-//     .card-img {
-//       width: 200px;
-//       height: 200px;
-//       border-radius: 50%;
-//       filter: brightness(80%);
-//     }
-//     .card-text {
-//       margin-top: 0.3rem;
-//       max-width: 35ch;
-//     }
-//   }
-// `;
 
 export default Card;
