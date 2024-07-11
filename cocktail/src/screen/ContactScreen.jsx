47f864e34d2b8ef1ea2087c746cbeabace492a18
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-11 17:49:24 +0200

    aggiunge il form - crea lo stile per il form

--- cocktail/src/screen/ContactScreen.jsx
+++ cocktail/src/screen/ContactScreen.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Footer, Hero, Navbar } from "../components";
+import { Footer, Hero, Navbar, Button } from "../components";
 import backImage from "../assets/image/Contact-hero.png";
+
 const ContactScreen = () => {
   return (
     <>
       <Navbar />
       <Hero img={backImage} disableOverlay>
-        <div className="flex h-screen flex-col p-8">
+        <div className="flex h-screen flex-col gap-8 p-8">
           {/* sezione info aggiunta cocktail */}
           <div className="grid gap-4 text-center">
             <h4 className="text-4xl font-bold tracking-wide text-slate-900">
@@ -19,7 +20,55 @@ const ContactScreen = () => {
             </p>
           </div>
           {/* form per aggiunta cocktail */}
-          <div className="h-96 w-full bg-red-500 "></div>
+          <div className="w-full rounded bg-white shadow-2xl drop-shadow-2xl">
+            <form className="flex flex-col gap-4 p-8">
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-semibold uppercase text-violet-800">
+                  nome
+                </label>
+                <input type="text" placeholder="inserisci il tuo nome" />
+                <hr />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-semibold uppercase text-violet-800">
+                  cognome
+                </label>
+                <input type="text" placeholder="inserisci il tuo cognome" />
+                <hr />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-semibold uppercase text-violet-800">
+                  email
+                </label>
+                <input type="email" placeholder="cocktail@example.com" />
+                <hr />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-semibold uppercase text-violet-800">
+                  telefono
+                </label>
+                <input type="tel" placeholder="333-333-333" />
+                <hr />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-semibold uppercase text-violet-800">
+                  la tua ricetta
+                </label>
+                <input type="text" placeholder="scrivi qui la tua ricetta" />
+                <hr />
+              </div>
+
+              <div className="flex w-full justify-center">
+                <Button buttonClass=" w-1/2 py-3">
+                  invia ricetta
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
       </Hero>
       <Footer />
