2b6f825bf93e913e76ed715429b9dc1c6416a24b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-12 13:22:15 +0200

    style: :art: cambia opacita
    cambia opacita di hero per una miglior leggibilita del testo

--- cocktail/src/screen/HomeScreen.jsx
+++ cocktail/src/screen/HomeScreen.jsx
@@ -12,22 +12,21 @@ const HomeScreen = () => {
       <Navbar />
       <Hero>
         <div className="grid h-screen place-items-center gap-4 p-8 text-white">
+          <div className="grid place-items-center gap-8 py-8 text-white">
+            <div className="grid gap-4 text-center font-bold uppercase tracking-wider">
+              <h2 className="text-5xl text-pink-500">Wiki drink</h2>
+              <h4>tutti i cocktail del mondo a portata di click</h4>
+            </div>
+            <h5 className="text-center text-lg">
+              Wiki Drink e un database internazionale che mette a tua
+              disposizione, in maniera Gratuita, le ricette dei piu importanti e
+              diffusi cocktail al mondo
+            </h5>
 
-        <div className="grid place-items-center gap-8 py-8 text-white">
-          <div className="grid gap-4 text-center font-bold uppercase tracking-wider">
-            <h2 className="text-5xl text-pink-500">Wiki drink</h2>
-            <h4>tutti i cocktail del mondo a portata di click</h4>
+            <Button>Scopri di piu</Button>
+
+            {/* immagine animata qui */}
           </div>
-          <h5 className="text-lg text-center ">
-            Wiki Drink e un database internazionale che mette a tua
-            disposizione, in maniera Gratuita, le ricette dei piu importanti e
-            diffusi cocktail al mondo
-          </h5>
-
-          <Button>Scopri di piu</Button>
-
-          {/* immagine animata qui */}
-        </div>
           <div className="h-60 w-60 bg-red-500" />
         </div>
       </Hero>
