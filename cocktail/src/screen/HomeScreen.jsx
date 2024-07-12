c25ffbdd92a644e1bd604a7eea1e53079cf39a12
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-12 13:20:07 +0200

    style: :art: aggiunge home
    aggiunge il testo alla homepage, aggiunge il bottone 'scopri di piu', prende spazio per l'animazione

--- cocktail/src/screen/HomeScreen.jsx
+++ cocktail/src/screen/HomeScreen.jsx
@@ -4,13 +4,33 @@
 // import Lottie from "react-lottie";
 // import animationData from "../assets/animation/drink-animation.json";
 // import { Link } from "react-router-dom";
-import { Footer, Navbar } from "../components";
+import { Button, Footer, Hero, Navbar } from "../components";
 // import Navbar from "../components/Navbar";
 const HomeScreen = () => {
   return (
     <>
       <Navbar />
-      <div className="">sono la homepage</div>
+      <Hero>
+        <div className="grid h-screen place-items-center gap-4 p-8 text-white">
+
+        <div className="grid place-items-center gap-8 py-8 text-white">
+          <div className="grid gap-4 text-center font-bold uppercase tracking-wider">
+            <h2 className="text-5xl text-pink-500">Wiki drink</h2>
+            <h4>tutti i cocktail del mondo a portata di click</h4>
+          </div>
+          <h5 className="text-lg text-center ">
+            Wiki Drink e un database internazionale che mette a tua
+            disposizione, in maniera Gratuita, le ricette dei piu importanti e
+            diffusi cocktail al mondo
+          </h5>
+
+          <Button>Scopri di piu</Button>
+
+          {/* immagine animata qui */}
+        </div>
+          <div className="h-60 w-60 bg-red-500" />
+        </div>
+      </Hero>
       <Footer />
     </>
   );
