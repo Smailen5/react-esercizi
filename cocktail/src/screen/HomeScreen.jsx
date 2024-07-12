98d58176ac061cb6a6890bf7c8b360e7e5b4bb53
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-12 14:22:49 +0200

    refactor: :art: aggiunge animazione
    aggiunge animaizone homepage, scaricato diversi json come animazione

--- cocktail/src/screen/HomeScreen.jsx
+++ cocktail/src/screen/HomeScreen.jsx
@@ -5,13 +5,15 @@
 // import animationData from "../assets/animation/drink-animation.json";
 import { Link } from "react-router-dom";
 import { Button, Footer, Hero, Navbar } from "../components";
+import animationData from "../assets/animation/Animation-cocktail-3.json";
+import Lottie from "react-lottie";
 // import Navbar from "../components/Navbar";
 const HomeScreen = () => {
   return (
     <>
       <Navbar />
       <Hero>
-        <div className="grid h-screen place-items-center gap-4 p-8 text-white font-Opensans">
+        <div className="font-Opensans grid h-screen place-items-center gap-4 p-8 text-white">
           <div className="grid place-items-center gap-8 py-8 text-white">
             <div className="grid gap-4 text-center font-bold uppercase tracking-wider">
               <h2 className="text-4xl text-pink-500">Wiki drink</h2>
@@ -19,15 +21,29 @@ const HomeScreen = () => {
             </div>
             <h5 className="text-center text-sm">
               Wiki Drink e un database internazionale che mette a tua
-              disposizione, in maniera <span className="text-pink-500 capitalize">gratuita</span>, le ricette dei piu importanti e
-              diffusi cocktail al mondo
+              disposizione, in maniera{" "}
+              <span className="capitalize text-pink-500">gratuita</span>, le
+              ricette dei piu importanti e diffusi cocktail al mondo
             </h5>
 
-            <Button buttonClass="py-3 px-6"><Link to="/about">Scopri di piu</Link></Button>
+            <Button buttonClass="py-3 px-6">
+              <Link to="/about">Scopri di piu</Link>
+            </Button>
 
             {/* immagine animata qui */}
           </div>
-          <div className="h-60 w-60 bg-red-500" />
+          <div>
+            <Lottie
+              options={{
+                loop: true,
+                autoplay: true,
+                animationData: animationData,
+                rendererSettings: {
+                  preserveAspectRatio: "xMidYMid slice",
+                },
+              }}
+            />
+          </div>
         </div>
       </Hero>
       <Footer />
