40a303f96e6af8f750de5b23ed4eea1b05095302
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-17 16:18:42 +0200

    feat: :art: aggiunge route
    aggiunge percorso al link del footer

--- cocktail/src/components/Footer.jsx
+++ cocktail/src/components/Footer.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer className="flex justify-center bg-violet-800 p-4">
       <div>
         <p className="text-sm font-semibold uppercase text-white">
           all © reserved to
-          {/* ricordati di collegare il link al 'team' */}
-          <a href="#" className="text-pink-500">
+          <Link to="/about" className="text-pink-500">
             {" "}
             drink team
-          </a>
+          </Link>
         </p>
       </div>
     </footer>
